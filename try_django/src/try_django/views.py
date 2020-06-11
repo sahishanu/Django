1a7bd96d48286d74a2736834c6a3f46de5c38873
root <sahiscord@gmail.com>
2020-06-11 23:25:56 +0530

    some modification

--- try_django/src/try_django/views.py
+++ try_django/src/try_django/views.py
@@ -2,12 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-	return render(request,"hello_world.html")
+	mytitle="Home"
+	return render(request,"page.html",{"title":mytitle})
 	
 
 def about_page(request):
-	return HttpResponse("<h1>About Us :</h1>")
+	mytitle="About Us"
+	return render(request,"page.html",{"title":mytitle})
 
 
 def contact_page(request):
-	return HttpResponse("<h1>Contact Us :</h1>")
+	mytitle="Contact Us"
+	return render(request,"page.html",{"title":mytitle})
